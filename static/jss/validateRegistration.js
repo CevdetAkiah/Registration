889bcb38a5088b72be6e4437ade31ceb73c5f5fb
Cevdet Akiah <cevdet.akiah@gmail.com>
2022-02-05 16:52:14 +0000

    added some more validation for the registration page. Still in progress

--- static/jss/validateRegistration.js
+++ static/jss/validateRegistration.js
@@ -1,3 +1,5 @@
+
+//validateForm checks if the password is equal to the repeat password fields
 function validateForm() {
     let x = document.forms["formReg"]["pwd"].value;
     let y = document.forms["formReg"]["pwd-repeat"].value;
@@ -5,9 +7,48 @@ function validateForm() {
         Swal.fire({
             icon: 'error',
             title: `Oops...`,
-            text: `Your password doesn't match`,
-            footer: '<a href="">Why do I have this issues?</a>'
+            text: `Your password doesn't match.`,
         })
+
         return false;
     }
 }
+
+//validateInput changes css styles depending on if the input is valid.
+function validateInput(x) {
+    let myEl = document.getElementById(x);
+    if (myEl.type == "email") {
+        if (!myEl.checkValidity()) {
+            myEl.classList.add("invalid");
+            myEl.classList.remove("valid");
+        } else {
+            myEl.classList.remove("invalid");
+            myEl.classList.add("valid");
+        }
+    } else {
+        if (!myEl.checkValidity()) {
+            myEl.classList.add("invalid");
+            myEl.classList.remove("valid");
+        } else {
+            myEl.classList.remove("invalid");
+            myEl.classList.add("valid");
+        }
+    }
+}
+
+
+
+
+// function inputValidate() {
+//     form = document.forms["formReg"];
+//     field = Array.from(form.elements);
+
+//     field.forEach(i => {
+//         i.setCustomValidity("");
+//         if (length(i) > 0) {
+//             if (!i.checkValidity()) {
+//                 i.classList.add('invalid');
+//             }
+//         }
+//     })
+// }
